85505dffe63bed6e9223b968e4c83b7e8f7c1e0f
Vladimir Ebel <vladimirebel@Vladimirs-MacBook-Air.local>
2024-09-23 16:56:12 +0700

    edited app.js

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const profileRoutes = require('./routes/profileRoutes');
-const saleRoutes = require('./routes/saleRoutes');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const saleRoutes = require("./routes/saleRoutes");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/sale', saleRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/contracts", express.static(path.join(__dirname, "contracts")));
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+app.use("/sale", saleRoutes);
 
 sequelize.sync().then(() => {
   app.listen(3000, () => {
-    console.log('Сервер запущен на порту 3000');
+    console.log("Сервер запущен на порту 3000");
   });
 });
