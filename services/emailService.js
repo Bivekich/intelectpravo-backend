b9af9cd1ba3add7d5610000022494bd043c0d5b1
Vladimir Ebel <vladimirebel@MacBook-Air-Vladimir.local>
2024-11-17 21:45:56 +0700

    Отключено ожидаение отправки письма

--- services/emailService.js
+++ services/emailService.js
@@ -4,19 +4,30 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: "smtp.timeweb.ru",
   port: 465,
-  secure: true,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
 });
 
-exports.sendEmail = async (email, subject, text) => {
+// Function to send email
+exports.sendEmail = (email, subject, text) => {
+  // Send email without waiting for the response
+  transporter
+    .sendMail({
+      from: process.env.EMAIL_USER, // Sender address
+      to: email, // List of recipients
+      subject: subject, // Subject line
+      text: text, // Plain text body
+    })
+    .then((info) => {
+      console.log("Email sent: " + info.response);
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+    });
+
+  // Immediately return true (indicating the function has completed)
   return true;
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: subject,
-    text: text,
-  });
 };
